fded69c998f682b843471e47b2036f12e0498c6d
sanketjariwal127 <sanketjariwala127@gmail.com>
2023-03-16 23:59:49 +0530

    fix logout routing issue

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,9 +1,8 @@
 import { defineStore } from "pinia";
 import { computed, ref } from "vue";
 import { useLocalStorage } from "@vueuse/core";
-import { useRouter } from "vue-router";
+import router from "../router";
 export const useAuthStore = defineStore('auth', () => {
-    const router = useRouter(); 
     const state = ref(useLocalStorage('auth', {
         user: null,
         token: null,
@@ -19,11 +18,15 @@ export const useAuthStore = defineStore('auth', () => {
     }
 
     const logout = () => {
-        state.value.user = null;
-        state.value.token = null;
-        state.value.email = null;
-        state.value.isAuth = false;
-        router.push({ name: "login" });
+        try {
+            state.value.user = null;
+            state.value.token = null;
+            state.value.email = null;
+            state.value.isAuth = false;
+            router.push({ name: 'login' });
+        } catch (error) {
+            console.log('error -->', error);
+        }
     }
 
     const getAuthDetails = computed(() => {
